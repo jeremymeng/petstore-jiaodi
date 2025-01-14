06f660f4dc1340a0a5b589be75eb4c82f5ad46b5
Jeremy Meng <jeremy.ymeng@gmail.com>
2025-01-14 10:29:13 -0800

    update for parcel

--- parcel/js/index.js
+++ parcel/js/index.js
@@ -1,16 +1,16 @@
-const { PetStoreClient } = require("@unbranded/petstore");
+import { PetStoreClient } from "@unbranded/petstore";
 
 const client = new PetStoreClient("http://localhost:5118", {
-    allowInsecureConnection: true,
-  });
+  allowInsecureConnection: true,
+});
 async function listPets() {
-    // list all pets
-    console.log("Listing all pets...");
-    const result = await client.pets.list();
-    console.log(result);
-  }
+  // list all pets
+  console.log("Listing all pets...");
+  const result = await client.pets.list();
+  console.log(result);
+}
 
 window.addEventListener("DOMContentLoaded", () => {
-    const listPetsButton = document.getElementById("listPets");
-    listPetsButton?.addEventListener("click", () => listPets());
+  const listPetsButton = document.getElementById("listPets");
+  listPetsButton?.addEventListener("click", () => listPets());
 });
